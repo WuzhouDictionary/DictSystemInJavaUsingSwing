7d70805bf88395f0df8b3dfc4f474d78164583c7
Zhizhong Yan <A2564011261@163.com>
2024-10-18 16:09:20 +0800

    Using Swing

--- src/main/java/top/mryan2005/managesysteminjava/SwingBootstrap.java
+++ src/main/java/top/mryan2005/managesysteminjava/SwingBootstrap.java
@@ -1,7 +1,7 @@
 package top.mryan2005.managesysteminjava;
 
-public class JavaFXBootstrap {
+public class SwingBootstrap {
     public static void main(String[] args) {
-        HelloApplication.main(args);
+        System.out.println("Hello, World!");
     }
 }

c1ba3d925a00bea2e52a9103c476c0b274f1d83b
Zhizhong Yan <A2564011261@163.com>
2024-10-19 12:34:22 +0800

    Can Send Issue to GitHub

--- src/main/java/top/mryan2005/managesysteminjava/Core.java
+++ src/main/java/top/mryan2005/managesysteminjava/Core.java
@@ -1,20 +1,30 @@
 package top.mryan2005.managesysteminjava;
+import top.mryan2005.managesysteminjava.ConnectToNet.POSTAndGET;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Core extends JFrame {
 
+    public void print(String str) {
+        System.out.println(str);
+    }
+
     public String printPath() {
-        String path = this.getClass().getClassLoader().getResource("").getPath();
+        String path = String.valueOf(this.getClass().getClassLoader().getResource(""));
+        if(path.matches("file:(.*)/classes/")) {
+            path = path.substring(6, path.length() - 15)+"/src/main/resources/top/mryan2005/managesysteminjava/";
+        } else if(path == "null") {
+            path = "";
+        }
         System.out.println(path);
         return path;
     }
 
-    public Core() {
-        super("Wuzhou Dictionary");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ImageIcon icon=new ImageIcon(printPath()+"/1.png");
+    public void buildWindows() {
+        ImageIcon icon=new ImageIcon(printPath()+"1.png");
+        print(printPath()+"1.png");
         JLabel label=new JLabel(icon);
         label.setBounds(0,0,1729,972);
         Container container = getContentPane();
@@ -27,6 +37,11 @@ public class Core extends JFrame {
         jMenuBar.add(jMenu);
         JMenuItem jMenuItemExit = new JMenuItem("Exit");
         jMenu.add(jMenuItemExit);
+        JMenu jMenu2 = new JMenu("About");
+        jMenuBar.add(jMenu2);
+        JMenuItem jMenuItemAbout = new JMenuItem("New Issue");
+        jMenu2.add(jMenuItemAbout);
+        jMenuItemAbout.addActionListener(this::newIssue);
         jMenuItemExit.addActionListener(this::actionPerformed);
         // build a dock bar like Mac OS
         // Create the dock bar
@@ -55,6 +70,11 @@ public class Core extends JFrame {
 
         // Add the dock bar to the bottom of the frame
         container.add(dockBar, BorderLayout.SOUTH);
+    }
+
+    public Core() {
+        super("Wuzhou Dictionary");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(0,0,1729,972);
     }
 
@@ -62,4 +82,59 @@ public class Core extends JFrame {
         System.exit(0);
     }
 
+    public void newIssue(ActionEvent e) {
+        JDialog jDialog = new JDialog(this, "New Issue", true);
+        jDialog.setBounds(0,0,500,500);
+        jDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jDialog.setLayout(new FlowLayout());
+        JLabel jLabel = new JLabel("Title");
+        JTextField jTextFieldTitle = new JTextField();
+        jTextFieldTitle.setSize(100, 50);
+        jTextFieldTitle.setColumns(20);
+        JLabel jLabel2 = new JLabel("Body");
+        JTextArea jTextAreaBody = new JTextArea();
+        jTextAreaBody.setSize(100, 50);
+        jTextAreaBody.setColumns(20);
+        JButton jButtonSubmit = new JButton("Submit");
+        jDialog.add(jLabel);
+        jDialog.add(jTextFieldTitle);
+        jDialog.add(jLabel2);
+        jDialog.add(jTextAreaBody);
+        jDialog.add(jButtonSubmit);
+        JDialog jDialog1 = new JDialog(this, "请输入验证码", true);
+        jDialog1.setLayout(new FlowLayout());
+        JLabel jLabelPW = new JLabel("验证码");
+        JTextField jTextFieldPW = new JTextField();
+        jTextFieldPW.setSize(100, 50);
+        jTextFieldPW.setColumns(20);
+        JLabel jLabelPW2 = new JLabel("验证码为随机生成的6位数字");
+        JButton jButton1 = new JButton("Submit");
+        jDialog1.add(jLabelPW);
+        jDialog1.add(jTextFieldPW);
+        jDialog1.add(jLabelPW2);
+        jDialog1.add(jButton1);
+        jButtonSubmit.addActionListener(e1 -> {
+            if (jTextFieldTitle.getText().equals("") || jTextAreaBody.getText().equals("")) {
+                JOptionPane.showMessageDialog(jDialog1, "Title or Body is empty");
+                return;
+            }
+            jDialog1.setBounds(0,0,500,500);
+            jDialog1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            Integer PW = new java.util.Random().nextInt(1000000);
+            jLabelPW2.setText("验证码为 "+PW.toString());
+            jButton1.addActionListener(e11 -> {
+                if (!jTextFieldPW.getText().equals(PW.toString())) {
+                    JOptionPane.showMessageDialog(jDialog1, "验证码错误");
+                    return;
+                }
+                POSTAndGET postAndGet = new POSTAndGET();
+                postAndGet.newIssue(jTextFieldTitle.getText(), jTextAreaBody.getText(), new String[]{"ReportedBugs"});
+                JOptionPane.showMessageDialog(jDialog1, "Issue Submitted");
+                jDialog1.dispose();
+                jDialog.dispose();
+            });
+            jDialog1.setVisible(true);
+        });
+        jDialog.setVisible(true);
+    }
 }
--- src/main/java/top/mryan2005/managesysteminjava/SwingBootstrap.java
+++ src/main/java/top/mryan2005/managesysteminjava/SwingBootstrap.java
@@ -1,10 +1,13 @@
 package top.mryan2005.managesysteminjava;
 
+import java.lang.*;
+
 public class SwingBootstrap {
 
     public static void main(String[] args) {
         Core core = new Core();
         String a = core.printPath();
+        core.buildWindows();
         core.setVisible(true);
     }
 }

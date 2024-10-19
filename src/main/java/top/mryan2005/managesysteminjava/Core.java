d1f1a93274ae41748327a5e1aeff04c058ae56ff
Zhizhong Yan <A2564011261@163.com>
2024-10-19 14:56:18 +0800

    change some params

--- src/main/java/top/mryan2005/managesysteminjava/Core.java
+++ src/main/java/top/mryan2005/managesysteminjava/Core.java
@@ -86,33 +86,95 @@ public class Core extends JFrame {
         JDialog jDialog = new JDialog(this, "New Issue", true);
         jDialog.setBounds(0,0,500,500);
         jDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        jDialog.setLayout(new FlowLayout());
+        jDialog.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        Insets insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = insets;
         JLabel jLabel = new JLabel("Title");
-        JTextField jTextFieldTitle = new JTextField();
+        JTextField jTextFieldTitle = new JTextField("", 20);
         jTextFieldTitle.setSize(100, 50);
         jTextFieldTitle.setColumns(20);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1; // 横占一个单元格
+        gbc.gridheight = 1; // 列占一个单元格
+        gbc.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog.add(jLabel, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1; // 横占一个单元格
+        gbc.gridheight = 1; // 列占一个单元格
+        gbc.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog.add(jTextFieldTitle, gbc);
         JLabel jLabel2 = new JLabel("Body");
-        JTextArea jTextAreaBody = new JTextArea();
+        JTextArea jTextAreaBody = new JTextArea(20, 20);
         jTextAreaBody.setSize(100, 50);
         jTextAreaBody.setColumns(20);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1; // 横占一个单元格
+        gbc.gridheight = 1; // 列占一个单元格
+        gbc.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog.add(jLabel2, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1; // 横占一个单元格
+        gbc.gridheight = 1; // 列占一个单元格
+        gbc.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog.add(jTextAreaBody, gbc);
         JButton jButtonSubmit = new JButton("Submit");
-        jDialog.add(jLabel);
-        jDialog.add(jTextFieldTitle);
-        jDialog.add(jLabel2);
-        jDialog.add(jTextAreaBody);
-        jDialog.add(jButtonSubmit);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 1; // 横占一个单元格
+        gbc.gridheight = 1; // 列占一个单元格
+        gbc.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog.add(jButtonSubmit, gbc);
         JDialog jDialog1 = new JDialog(this, "请输入验证码", true);
-        jDialog1.setLayout(new FlowLayout());
+        jDialog1.setLayout(new GridBagLayout());
+        Insets insets1 = new Insets(10, 10, 10, 10);
+        GridBagConstraints gbc1 = new GridBagConstraints();
+        gbc1.fill = GridBagConstraints.BOTH;
+        gbc1.insets = insets1;
         JLabel jLabelPW = new JLabel("验证码");
-        JTextField jTextFieldPW = new JTextField();
+        gbc1.gridx = 0;
+        gbc1.gridy = 0;
+        gbc1.gridwidth = 1; // 横占一个单元格
+        gbc1.gridheight = 1; // 列占一个单元格
+        gbc1.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc1.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog1.add(jLabelPW, gbc1);
+        JTextField jTextFieldPW = new JTextField("", 20);
+        gbc1.gridx = 1;
+        gbc1.gridy = 0;
+        gbc1.gridwidth = 1; // 横占一个单元格
+        gbc1.gridheight = 1; // 列占一个单元格
+        gbc1.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc1.weighty = 0.0; // 当窗口放大时，高度不变
         jTextFieldPW.setSize(100, 50);
         jTextFieldPW.setColumns(20);
+        jDialog1.add(jTextFieldPW, gbc1);
         JLabel jLabelPW2 = new JLabel("验证码为随机生成的6位数字");
+        gbc1.gridx = 0;
+        gbc1.gridy = 1;
+        gbc1.gridwidth = 2; // 横占一个单元格
+        gbc1.gridheight = 1; // 列占一个单元格
+        gbc1.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc1.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog1.add(jLabelPW2, gbc1);
         JButton jButton1 = new JButton("Submit");
-        jDialog1.add(jLabelPW);
-        jDialog1.add(jTextFieldPW);
-        jDialog1.add(jLabelPW2);
-        jDialog1.add(jButton1);
+        gbc1.gridx = 1;
+        gbc1.gridy = 2;
+        gbc1.gridwidth = 1; // 横占一个单元格
+        gbc1.gridheight = 1; // 列占一个单元格
+        gbc1.weightx = 0.0; // 当窗口放大时，长度不变
+        gbc1.weighty = 0.0; // 当窗口放大时，高度不变
+        jDialog1.add(jButton1, gbc1);
         jButtonSubmit.addActionListener(e1 -> {
             if (jTextFieldTitle.getText().equals("") || jTextAreaBody.getText().equals("")) {
                 JOptionPane.showMessageDialog(jDialog1, "Title or Body is empty");
